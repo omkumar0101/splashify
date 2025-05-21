901a1d42dba4e0943af960cfe0f6b07ab460cbe7
omkumar0101 <sadhdahj@gmail.com>
2025-05-21 14:26:51 +0530

    Update wallet connection files

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const path = require('path');
 const { serve } = require('https-localhost');
 const app = express();
 
-// Serve static files
 app.use(express.static(path.join(__dirname)));
 
-// Serve index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start HTTPS server
 const port = 3000;
 serve(app, { port }).listen(port, () => {
   console.log(`Server running at https://localhost:${port}`);
